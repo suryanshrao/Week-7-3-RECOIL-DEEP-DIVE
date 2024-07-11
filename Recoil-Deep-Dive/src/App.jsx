71b56e275af7cf1a5357b00b8c329eb83d20dd73
Suryansh <suryanshrao01@gmail.com>
2024-07-12 02:00:05 +0530

    Understanding Recoil Better

---
+++ Recoil-Deep-Dive/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div>
+      <button>Home</button>
+      <button>My Network</button>
+      <button>Jobs</button>
+      <button></button>
+      <button></button>
+      <button></button>
+    </div>
+  )
+}
+
+export default App
