6090f90e969560614c73dbdd97fc0bfd8998373c
Suryansh <suryanshrao01@gmail.com>
2024-07-12 02:20:09 +0530

    Imported every atom from the Store

--- Recoil-Deep-Dive/src/App.jsx
+++ Recoil-Deep-Dive/src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
+import { useRecoilValue } from 'recoil'
+import { jobsAtom, mesageAtom, networkAtom, notificationsAtom } from './store/atoms/context'
 
 function App() {
+
+  const networkCount = useRecoilValue(networkAtom);
+  const jobCount = useRecoilValue(jobsAtom);
+  const messageCount = useRecoilValue(mesageAtom);
+  const notificationsCount = useRecoilValue(notificationsAtom);
 
   return (
     <div>
       <button>Home</button>
-      <button>My Network[]</button>
-      <button>Jobs []</button>
-      <button>Messaging []</button>
-      <button>Notifications [] </button>
+      <button>My Network [{networkCount}]</button>
+      <button>Jobs [{jobCount}]</button>
+      <button>Messaging [{messageCount}]</button>
+      <button>Notifications [{notificationsCount}] </button>
       <button> Me </button>
     </div>
   )
