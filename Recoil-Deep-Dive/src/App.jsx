0e568a7a19e9ce268a0e0713ec88aa9e554c2dfc
Suryansh <suryanshrao01@gmail.com>
2024-07-12 02:15:48 +0530

    Created context apis

--- Recoil-Deep-Dive/src/App.jsx
+++ Recoil-Deep-Dive/src/App.jsx
@@ -5,11 +5,11 @@ function App() {
   return (
     <div>
       <button>Home</button>
-      <button>My Network</button>
-      <button>Jobs</button>
-      <button></button>
-      <button></button>
-      <button></button>
+      <button>My Network[]</button>
+      <button>Jobs []</button>
+      <button>Messaging []</button>
+      <button>Notifications [] </button>
+      <button> Me </button>
     </div>
   )
 }
---
+++ Recoil-Deep-Dive/src/store/atoms/context.jsx
@@ -0,0 +1,22 @@
+import {atom} from "recoil"
+
+export const networkAtom = atom({
+    key : "networkAtom",
+    default : 120
+})
+
+export const jobsAtom  = atom({
+    key : "jobsAtom",
+    default : 0
+})
+
+export const mesageAtom = atom({
+    key : "messageAtom",
+    default : 0
+})
+
+export const notificationsAtom = atom({
+    key : "notificationsAtom",
+    default : 12
+})
+
